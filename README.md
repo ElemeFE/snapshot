eddc5a9036dd53d290a0a9a33deb6bf1d43f34a0
baiyaaaaa <bigpet1991@gmail.com>
2015-11-26 18:23:50 +0800

    first commit

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-＃快照
+# 快照
 
-一个小工具，可以为你页面中需要异步加载数据才渲染的dom片段存储到localStorage中，下次进入页面时可直接先显示localStorage中的快照，再切换为异步渲染出来的片段。从而增强用户体验。
+一个小工具，可以为你页面中需要异步加载数据才渲染的dom片段存储到localStorage中，下次进入页面时可直接先显示localStorage中的快照，再切换为数据拉去完毕后异步渲染出来的片段。从而增强用户体验。
 
 ### 1. 使用说明
 
-只要在你想要进行快照显示的文档的父节点标签上添加`data-observer="fragmentName"`即可，例如：
+只要在你想要进行快照显示的文档的父节点标签上添加`data-snapshot="fragmentName"`即可，例如：
 
-	<div class="order-detail" data-observer="order-detail">
+	<div class="order-detail" data-snapshot="order-detail">
   		<h3 class="title">{{title}}</h3>
   		<div class="total">{{total}}</div>
   		<div class="date">{{date}}</div>
--- index.js
+++ index.js
@@ -1,76 +1,84 @@
 ;(function() {
-  'use strict'
+  'use strict';
 
   if (document.readyState === 'complete') {
     // 如果文档已完成接收，则直接调用
-    setTimeout(go, 1);
+    setTimeout(go);
   } else {
     // 否则监听 DOMContentLoaded 事件
-    document.addEventListener('DOMContentLoaded', go, false);
+    document.addEventListener('DOMContentLoaded', go);
   }
 
   function go() {
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var nodes = document.querySelectorAll('[data-snapshot]');
+    var observer;
 
     // 创建观察者对象
     try {
-      var observer = new MutationObserver(function(mutations) {
-          var node = mutations[0].target;
-          var name = node.getAttribute('data-observer') || getNodePath(node);
-          var newFragment = node.innerHTML;
-          var comments = '<!-- ' + new Date().getTime() + ' -->';
-
-          localStorage.setItem(name, comments + newFragment);
-          this.disconnect();
-      });
+      observer = new MutationObserver(observerCB);
     } catch(e) {}
 
-    var nodes = document.querySelectorAll('[data-observer]');
-
-    // 遍历具有data-observer属性的节点并将其内容替换为上一次存储在本地的快照
+    // 遍历具有data-snapshot属性的节点并将其内容替换为上一次存储在本地的快照
     for (var i = nodes.length - 1; i >= 0; i--) {
-      var name = nodes[i].getAttribute('data-observer') || getNodePath(nodes[i]);
-      var oldFragment = localStorage.getItem(name) || '';
 
-      nodes[i].innerHTML = oldFragment;
+      nodes[i].innerHTML = getSnapshot(nodes[i]);
 
       // 对不支持MutationObserver的浏览器做兼容
       if (observer && observer.observer) {
-        observer.observe(nodes[i], { childList:true, subtree: true });
+        observer.observe(nodes[i], { childList: true, subtree: true });
       } else {
-        observeDom(nodes[i], oldFragment);
+        observeDom(nodes[i]);
       }
     }
   }
 
+  function observerCB(mutations) {
+    var node = mutations[0].target;
+    saveSnapshot(node);
+    this.disconnect();
+  }
+
   function getNodePath(node) {
     var res = [];
+    var tempNode;
+    var i;
 
     while(node) {
-      var tempNode = node;
+      tempNode = node;
 
-      for (var i = 0; tempNode; i++) {
-        tempNode = tempNode.previousElementSibling || null;
+      for (i = 0; tempNode; i++) {
+        tempNode = tempNode.previousElementSibling;
       }
       res.push(i);
-      node = node.parentElement || null;
+      node = node.parentNode;
     }
 
     return res.join('-');
   }
 
-  function observeDom(node, oldFragment) {
-    setTimeout(function() {
-      var newFragment = node.innerHTML;
+  function observeDom(node) {
+    var oldFragment = getSnapshot(node);
 
-      if (oldFragment == newFragment) {
+    if (oldFragment === node.innerHTML) {
+      setTimeout(function() {
         observeDom(node, oldFragment);
-      } else {
-        // 如果观察片段发生变化，将最新的片段存储到localStorage
-        var name = node.getAttribute('data-observer') || getNodePath(nodes[i]);
-        var comments = '<!-- ' + new Date().getTime() + ' -->';
-        localStorage.setItem(name, comments + newFragment);
-      }
-    }, 100);
+      }, 500);
+    } else {
+      // 如果观察片段发生变化，将最新的片段存储到localStorage
+      saveSnapshot(node);
+    }
+  }
+
+  function getSnapshot(node) {
+    var name = node.getAttribute('data-snapshot') || getNodePath(node);
+
+    return localStorage.getItem(location.pathname + name) || '';
+  }
+
+  function saveSnapshot(node) {
+    var name = node.getAttribute('data-snapshot') || getNodePath(node);
+    var comments = '<!-- ' + new Date().getTime() + ' -->';
+    localStorage.setItem(location.pathname + name, comments + node.innerHTML);
   }
 })();
